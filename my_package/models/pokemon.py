f1bc270fb4aa733243666b19b590ddec69f34029
SCoriolan <stalenes.coriolan@gmail.com>
2025-10-28 12:30:10 +0100

    refactor: enhance Pokemon class with efficiency table and dynamic attribute calculations based on level

--- my_package/models/pokemon.py
+++ my_package/models/pokemon.py
@@ -1,12 +1,27 @@
 class Pokemon:
-    def __init__(self, name, type_pokemon, hp, attack, defense, speed, level=1):
+
+    """Basis class for Pokémon"""
+    
+    # Efficiency Table
+    EFFICIENCY = {
+        'Fire': {'Plant': 2.0, 'Water': 0.5, 'Fire': 1.0},
+        'Eau': {'Fire': 2.0, 'Plant': 0.5, 'Water': 1.0},
+        'Plant': {'Water': 2.0, 'Fire': 0.5, 'Plant': 1.0}
+    }
+
+    def __init__(self, name, type_pokemon,level=5):
         self.name = name
         self.type_pokemon = type_pokemon
-        self.hp = hp
-        self.attack = attack
-        self.defense = defense
-        self.speed = speed
         self.level = level
+        
+        self.hp_max     = 20 + (level *5)
+        self.hp_actuals = self.hp_max
+        self.attack     = 5 + (level *2)
+        self.defense    = 3 + level
+        self.speed      = 3 + level
+
+        self.ko = False
+      
         
     def __str__(self):
         return f"{self.name} ({self.type_pokemon}) - HP: {self.hp}, Attack: {self.attack}, Defense: {self.defense}, Speed: {self.speed}, Level: {self.level}"
