a890237e10260db7bfebceb2427ebc5a8f8f14bd
SCoriolan <stalenes.coriolan@gmail.com>
2025-10-28 12:11:00 +0100

    refactor: Pokemon class to rename 'type' attribute to 'type_pokemon' for clarity in initialization and string representation

--- my_package/models/pokemon.py
+++ my_package/models/pokemon.py
@@ -1,7 +1,7 @@
 class Pokemon:
-    def __init__(self, name, type, hp, attack, defense, speed, level=1):
+    def __init__(self, name, type_pokemon, hp, attack, defense, speed, level=1):
         self.name = name
-        self.type = type
+        self.type_pokemon = type_pokemon
         self.hp = hp
         self.attack = attack
         self.defense = defense
@@ -9,7 +9,7 @@ class Pokemon:
         self.level = level
         
     def __str__(self):
-        return f"{self.name} ({self.type}) - HP: {self.hp}, Attack: {self.attack}, Defense: {self.defense}, Speed: {self.speed}, Level: {self.level}"
+        return f"{self.name} ({self.type_pokemon}) - HP: {self.hp}, Attack: {self.attack}, Defense: {self.defense}, Speed: {self.speed}, Level: {self.level}"
     
     def __repr__(self):
-        return f"Pokemon({self.name}, {self.type}, {self.hp}, {self.attack}, {self.defense}, {self.speed}, {self.level})"
+        return f"Pokemon({self.name}, {self.type_pokemon}, {self.hp}, {self.attack}, {self.defense}, {self.speed}, {self.level})"
