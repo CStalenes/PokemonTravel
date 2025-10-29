30edc3eab016c8c2614e386ba6ecc0b577e084cf
SCoriolan <stalenes.coriolan@gmail.com>
2025-10-29 22:30:03 +0100

    feat: add display_menu and display_framed_message functions to enhance terminal output with structured menus and framed messages

--- utils/display.py
+++ utils/display.py
@@ -42,3 +42,38 @@ def display_separator(character="=", width=70):
         width (int): Width of the line
     """
     print(character * width)
+
+def display_menu(options, title=None):
+    """
+    Display a menu with numbered options
+    
+    Args:
+        options (list): List of options to display
+        title (str, optional): Title of the menu
+    """
+    if title:
+        display_title(title)
+    
+    print()
+    for i, option in enumerate(options, 1):
+        print(f"{i}. {option}")
+
+
+def display_framed_message(message, width=70, character="*"):
+    """
+    Display a message in a frame
+    
+    Args:
+        message (str): Message to display
+        width (int): Width of the frame
+        character (str): Character for the frame
+    """
+    print("\n" + character * width)
+    
+    # Gérer les messages multi-lignes
+    lines = message.split("\n")
+    for line in lines:
+        padding = (width - len(line) - 4) // 2
+        print(f"{character} {' ' * padding}{line}{' ' * (width - len(line) - padding - 4)} {character}")
+    
+    print(character * width + "\n")
